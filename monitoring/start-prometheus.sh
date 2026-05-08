dd782013bc8b14737923fcf406d42716b2b9f557
Elbouti <elboutisoufiane69@gmail.com>
2026-05-09 01:42:29 +0200

    Update and rename start-prometheus.sh to monitoring-stack.yml

--- monitoring/start-prometheus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo docker rm -f prometheus 2>/dev/null
-
-sudo docker run -d --name prometheus \
-  --network overlay-5g \
-  -p 9090:9090 \
-  -v ~/5g-cloud-lab/monitoring/prometheus.yml:/etc/prometheus/prometheus.yml \
-  --restart always \
-  prom/prometheus:latest
